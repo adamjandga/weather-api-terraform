2989903695126864b25010a1268bfe109f557336
Adam Jandga <44347366+adamjandga@users.noreply.github.com>
2024-05-22 17:16:06 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,40 +1,186 @@
 provider "aws" {
-  region = "us-east-1"  # Change this if needed
+  region = "us-east-1"
 }
 
 resource "aws_ecr_repository" "weather_app" {
   name = "weather-app"
 }
 
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "weather-app"
-  cluster_version = "1.21"
-  
-  # Replace with your actual VPC ID and subnet IDs
-  vpc_id          = "vpc-0170b8d55652ec6f9"
-  subnets         = ["subnet-0dd3c9084fffd80b7", "subnet-071b6572fe6bde046"]
+resource "aws_security_group" "eks_cluster" {
+  name        = "eks-cluster-sg"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = "vpc-0170b8d55652ec6f9"
 
-  node_groups = {
-    weather_app_nodes = {
-      desired_capacity = 1
-      max_capacity     = 10
-      min_capacity     = 1
-      instance_type    = "t3.medium"
+  tags = {
+    Name = "eks-cluster-sg"
+  }
+}
 
-      # Optional tags for the node group
-      tags = {
-        Name        = "weather-app-node-group"
-        Environment = "production"
-      }
+resource "aws_security_group_rule" "cluster_inbound" {
+  description              = "Allow worker nodes to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "cluster_outbound" {
+  description              = "Allow cluster API Server to communicate with the worker nodes"
+  from_port                = 1024
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "egress"
+}
+
+resource "aws_security_group" "eks_nodes" {
+  name        = "eks-nodes-sg"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = "vpc-0170b8d55652ec6f9"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-nodes-sg"
+    "kubernetes.io/cluster/weather-app" = "owned"
+  }
+}
+
+resource "aws_security_group_rule" "nodes" {
+  description              = "Allow nodes to communicate with each other"
+  from_port                = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "nodes_inbound" {
+  description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+  from_port                = 1025
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_cluster.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "weather-app-cluster"
+
+  assume_role_policy = data.aws_iam_policy_document.assume_cluster.json
+}
+
+resource "aws_iam_role_policy_attachment" "aws_eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+data "aws_iam_policy_document" "assume_cluster" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
     }
   }
-  
-  # Optional tags for the cluster
-  tags = {
-    Environment = "production"
-    Project     = "WeatherApp"
+}
+
+resource "aws_eks_cluster" "main" {
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    security_group_ids      = [aws_security_group.eks_cluster.id, aws_security_group.eks_nodes.id]
+    endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
+    subnet_ids = var.eks_cluster_subnet_ids
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.aws_eks_cluster_policy
+  ]
+}
+
+resource "aws_iam_role" "eks_nodes" {
+  name = "weather-app-worker"
+
+  assume_role_policy = data.aws_iam_policy_document.assume_workers.json
+}
+
+data "aws_iam_policy_document" "assume_workers" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "aws_eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "aws_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = var.node_group_name
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = var.eks_cluster_subnet_ids
+
+  ami_type       = var.ami_type
+  disk_size      = var.disk_size
+  instance_types = var.instance_types
+
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  tags = {
+    Name = var.node_group_name
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.aws_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.aws_eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_read_only,
+  ]
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "kubeconfig" {
+  value = aws_eks_cluster.main.kubeconfig
 }
 
 output "repository_url" {
