2a6319181c69a24ee52ebcdfd7824f6bf62506ab
Adam Jandga <44347366+adamjandga@users.noreply.github.com>
2024-05-22 17:18:52 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,8 +2,12 @@ output "cluster_endpoint" {
   value = aws_eks_cluster.main.endpoint
 }
 
-output "kubeconfig" {
-  value = aws_eks_cluster.main.kubeconfig
+output "cluster_certificate_authority" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.main.name
 }
 
 output "repository_url" {
