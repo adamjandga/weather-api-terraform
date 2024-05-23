804bb77293e9505db036890d18014cf9b9fe6b6c
Adam Jandga <44347366+adamjandga@users.noreply.github.com>
2024-05-22 17:15:39 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,11 @@
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value = aws_eks_cluster.main.endpoint
 }
 
 output "kubeconfig" {
-  value = module.eks.kubeconfig
+  value = aws_eks_cluster.main.kubeconfig
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.weather_app.repository_url
 }
